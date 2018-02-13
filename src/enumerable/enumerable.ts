de5862e989f95159e71f97df9bb6df82ffcc3dce
Victor Timoshin <victor-timoshin@hotmail.com>
2018-02-13 11:48:04 +0300

    Добавлена функция сравнения типа с целочисленным значением

--- src/enumerable/enumerable.ts
+++ src/enumerable/enumerable.ts
@@ -4,4 +4,16 @@ export namespace Enum {
 	export const toString = (enumerable: any, key: any): string => {
 		return enumerable[key] as string;
 	}
+
+	/**
+	 * Сравнивает целочисленное значение с типом
+	 * @param enumerable Типы
+	 * @param key Конкретный тип
+	 * @param value Целочисленное значение, которое следует сравнить с типом
+	 * 
+	 * @example Enum.compareWithNum(Types, Types.Red, this.state.selected_color)
+	 */
+	export const compareWithNum = (enumerable: any, key: any, value: number): boolean => {
+		return toString(enumerable, key) === enumerable[value];
+	}
 }
