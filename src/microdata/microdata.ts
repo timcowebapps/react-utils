fd6c7eadce7f8cfb0507abe73374886617dae10b
Victor Timoshin <victor-timoshin@hotmail.com>
2018-02-18 06:37:42 +0300

    Добавлен интерфейс ImageObject микроданных

---
+++ src/microdata/microdata.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+export namespace IMicrodata {
+	export interface ImageObject {
+		name: string;
+		author: string;
+		contentLocation: string;
+		description: string;
+		datePublished: string;
+	}
+}
